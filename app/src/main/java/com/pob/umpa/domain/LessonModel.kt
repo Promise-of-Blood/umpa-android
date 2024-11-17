93645edd58af6fdccf65b78b73cc244b43587892
kdbswo <kdbswo@gmail.com>
2024-11-18 01:14:04 +0900

    Refactor: MatchingModel 수정

--- app/src/main/java/com/pob/umpa/domain/LessonModel.kt
+++ app/src/main/java/com/pob/umpa/domain/LessonModel.kt
@@ -1,12 +1,13 @@
 package com.pob.umpa.domain
 
-data class LessonModel(
+data class MatchingModel(
     val lessonId: String,
     val teacherId: String,
+    val teacherName: String,
     val title: String,
     val content: String,
     val price: Int,
-    val stars: Int,
+    val stars: Float,
     val matchingType: String,
     val instrumentsType: String,
     val locationCity: String,
