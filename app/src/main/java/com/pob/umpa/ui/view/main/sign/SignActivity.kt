18a3b89f21edfdfbbc2dd346c7c81db2a36b231c
kdbswo <kdbswo@gmail.com>
2024-11-18 21:09:16 +0900

    Add: SignActivity

---
+++ app/src/main/java/com/pob/umpa/ui/view/main/sign/SignActivity.kt
@@ -0,0 +1,31 @@
+package com.pob.umpa.ui.view.main.sign
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.pob.umpa.ui.theme.UmpaTheme
+
+class SignActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            UmpaTheme {
+                SignInScreen(navController)
+            }
+        }
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        UmpaTheme {
+            val navController = rememberNavController()
+            SignInScreen(navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/pob/umpa/ui/view/main/sign/SignInScreen.kt
@@ -0,0 +1,27 @@
+package com.pob.umpa.ui.view.main.sign
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.pob.umpa.ui.theme.UmpaTheme
+
+@Composable
+fun SignInScreen(navController: NavHostController) {
+    Column {
+        //Todo Logo
+        //Todo SignIn
+        //Todo find Id/password
+        //Todo Social Login
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignInScreenPreview() {
+    UmpaTheme {
+        val navController = rememberNavController()
+        SignInScreen(navController)
+    }
+}
