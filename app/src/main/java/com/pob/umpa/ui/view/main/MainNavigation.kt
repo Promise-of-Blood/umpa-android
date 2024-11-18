3ef10ef3014632b44d2aee87ee420520d6367c08
idkos <leegang31@gmail.com>
2024-11-18 18:22:32 +0900

    Refactor: 커뮤니티 Nav 추가

--- app/src/main/java/com/pob/umpa/ui/view/main/MainNavigation.kt
+++ app/src/main/java/com/pob/umpa/ui/view/main/MainNavigation.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.pob.umpa.ui.view.main.community.CommunityScreen
 import com.pob.umpa.ui.view.main.home.HomeScreen
 
 @Composable
@@ -23,7 +24,7 @@ fun MainNavigation(
         }
         composable("community") {
             // 커뮤니티 부분 스크린
-            Greeting(name = "커뮤니티")
+            CommunityScreen(modifier)
         }
         composable("chatting") {
             // 채팅 부분 스크린
