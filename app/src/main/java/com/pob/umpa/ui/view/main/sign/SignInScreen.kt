d8187e317e50ed2044904de0e9943928d801ef29
kdbswo <kdbswo@gmail.com>
2024-11-24 01:55:19 +0900

    Add: naver sign in icon

--- app/src/main/java/com/pob/umpa/ui/view/main/sign/SignInScreen.kt
+++ app/src/main/java/com/pob/umpa/ui/view/main/sign/SignInScreen.kt
@@ -1,11 +1,17 @@
 package com.pob.umpa.ui.view.main.sign
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.pob.umpa.R
 import com.pob.umpa.ui.theme.UmpaTheme
 
 @Composable
@@ -13,15 +19,24 @@ fun SignInScreen(navController: NavHostController) {
     Column {
         //Todo Logo
         //Todo SignIn
-
+        SignIn(modifier = Modifier.fillMaxSize())
         //Todo find Id/password
         //Todo Social Login
     }
 }
 
 @Composable
-fun SignIn(modifier: Modifier = Modifier) {
-
+fun SignIn(modifier: Modifier) {
+    Column(
+        modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.login_naver),
+            contentDescription = "네이버 로그인"
+        )
+    }
 }
 
 @Preview(showBackground = true)
