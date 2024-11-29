ec2c00893a723567723ac45f8e1df3addf94f444
overtae <rlaxodud911@gmail.com>
2024-11-29 22:38:31 +0900

    Refactor: 사용하지 않는 import문 제거

--- app/src/main/java/com/pob/umpa/ui/view/main/matching/detail/component/card/ImageSlideCard.kt
+++ app/src/main/java/com/pob/umpa/ui/view/main/matching/detail/component/card/ImageSlideCard.kt
@@ -22,9 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.pob.umpa.R
 import com.pob.umpa.ui.theme.UmpaColor
 import java.util.UUID
 
