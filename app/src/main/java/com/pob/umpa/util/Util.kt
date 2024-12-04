0d627d732811d768d8a447e4cfdf7f71ec9d004f
overtae <rlaxodud911@gmail.com>
2024-12-04 23:42:15 +0900

    Feature: 날짜 포맷팅 및 videoId 추출 유틸 함수 작성

--- app/src/main/java/com/pob/umpa/util/Util.kt
+++ app/src/main/java/com/pob/umpa/util/Util.kt
@@ -1,6 +1,9 @@
 package com.pob.umpa.util
 
 import android.icu.text.DecimalFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 /**
  * 1000원 단위로 (,)를 찍어주는 함수
@@ -10,3 +13,34 @@ import android.icu.text.DecimalFormat
 fun Int.toCommaString(): String {
     return DecimalFormat("#,###").format(this)
 }
+
+fun LocalDateTime.toFormattedString(pattern: String = "yyyy.MM.dd") =
+    this.format(DateTimeFormatter.ofPattern(pattern))
+
+fun LocalDateTime.toDiffString(): String {
+    val now = LocalDateTime.now()
+
+    val secondsDiff = ChronoUnit.SECONDS.between(this, now)
+    val minutesDiff = secondsDiff / 60
+    val hoursDiff = minutesDiff / 60
+    val daysDiff = hoursDiff / 24
+    val weeksDiff = daysDiff / 7
+    val monthsDiff = daysDiff / 30
+    val yearsDiff = daysDiff / 365
+
+    return when {
+        secondsDiff < 60 -> "${secondsDiff}초 전"
+        minutesDiff < 60 -> "${minutesDiff}분전"
+        hoursDiff < 24 -> "${hoursDiff}시간전"
+        daysDiff < 7 -> "${daysDiff}일전"
+        weeksDiff < 4 -> "${weeksDiff}주전"
+        monthsDiff < 12 -> "${monthsDiff}달전"
+        else -> "${yearsDiff}년전"
+    }
+}
+
+/**
+* Youtube 링크의 videoId를 추출해주는 함수
+* ex) https://www.youtube.com/watch?v=3DeTW8FKKYg -> 3DeTW8FKKYg
+* */
+fun String.parseVideoLink() = this.split("v=")[1].split("&")[0]
